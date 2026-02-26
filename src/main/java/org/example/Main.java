b5e9c9915d8c806063363e04cd802c43fdc95e11
Kristy Heijenk <ksheijenk@hotmail.com>
2026-02-26 11:13:02 +0100

    added a do while loop so the calculator can continue.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,39 @@
 package org.example;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Write a number");
-        double number1 = scanner.nextDouble();
-        System.out.println("Write a second number");
-        double number2 = scanner.nextDouble();
-
         Calculator calculator = new Calculator();
+        String choice = "yes";
 
-        System.out.println("Choose an operation (+ - * /)");
-        char operator = scanner.next().charAt(0);
-        double result;
+        do {
+            System.out.println("Write a number");
+            double number1 = scanner.nextDouble();
+            System.out.println("Write a second number");
+            double number2 = scanner.nextDouble();
 
-        if (operator == '+') {
-            result = calculator.add(number1, number2);
-        } else if (operator == '-') {
-            result = calculator.subtract(number1, number2);
-        } else if (operator == '*') {
-            result = calculator.multiply(number1, number2);
-        } else if (operator == '/') {
-            result = calculator.divide(number1, number2);
-        } else {
-            System.out.println("Invalid operator");
-            return;
-        }
-        System.out.println(number1 + " " + operator + " " + number2 + " = " + result);
+            System.out.println("Choose an operation (+ - * /)");
+            char operator = scanner.next().charAt(0);
+            double result;
+
+            if (operator == '+') {
+                result = calculator.add(number1, number2);
+            } else if (operator == '-') {
+                result = calculator.subtract(number1, number2);
+            } else if (operator == '*') {
+                result = calculator.multiply(number1, number2);
+            } else if (operator == '/') {
+                result = calculator.divide(number1, number2);
+            } else {
+                System.out.println("Invalid operator");
+                continue;
+            }
+
+            System.out.println(number1 + " " + operator + " " + number2 + " = " + result);
+            System.out.println("Do you want to perform another calculation? (yes/no)");
+            choice = scanner.next();
+
+        } while (choice.equalsIgnoreCase("yes"));
     }
 }
