661a0cf516649f884127b30b4c36bee6408c0216
Kristy Heijenk <ksheijenk@hotmail.com>
2026-03-02 11:09:11 +0100

    Resolved merge conflicts

--- src/main/java/org/example/Calculator2.java
+++ src/main/java/org/example/Calculator2.java
@@ -16,11 +16,6 @@ public class Calculator2 {
     Color customBlack = new Color(255,149,0);
 
 
-    JFrame frame = new JFrame("Calculator =)");
-    JLabel displayLabel = new JLabel();
-    JPanel displayPanel = new JPanel();
-    JPanel buttonsPanel = new JPanel();
-
     String[] buttonValues = {
             "AC", "+/-", "%", "÷",
             "7", "8", "9", "×",
@@ -30,6 +25,17 @@ public class Calculator2 {
     };
     String[] rightSymbols = {"÷", "×", "-", "="};
     String[] topSymbols = "AC", "+/-", "%"};
+
+    JFrame frame = new JFrame("Calculator =)");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
+    JPanel buttonsPanel = new JPanel();
+
+    //A+B, A-B, A*B, A/B
+    String A = "0";
+    String operator = null;
+    String B = null;
+
 
     Calculator()
         frame.setVisible(true);
@@ -58,5 +64,85 @@ public class Calculator2 {
             String buttonValue = buttonValues[i];
             button.setFont(new Font("Arial", Font.PLAIN, 30));
             button.setText(buttonValue);
+            button,setFocusable(false);
+            button.setBorder(new LineBorder(customBlack));
+
+
+
+            if (Arrays.asList(topSymbols).contains (buttonValue)){
+                button.setBackground(customLightGray);
+                button.setForeground(customBlack);
+            }
+            else if (Arrays.asList(rightSymbols).contains(buttonValue)){
+                button.setBackground(customOrange);
+                button.setForeground(Color.white);
+            }
+            else {button.setBackground(customDarkGray);
+            button.setForeground(Color.white);
+            }
+
+            buttonsPanel.add(button);
+
+            button.addActionListener(new ActionListener () {
+    public void actionPerformed (ActionEvent e){
+        JButton button = (JButton) e.getSource();
+        String buttonValue = button.getText();
+        if (Arrays.asList(rightSymbols).contains(buttonValue)) {
+            if (buttonValue == "=") {
+        }
+            else if ("+-×÷".contains(buttonValue)) {
+                if(operator == null) {
+                    A = displayLabel.getText();
+                    displayLabel.setText("0");
+                    B = "0";
+                }
+                operator = buttonValue;
+            }
+
+        } else if (Arrays.asList(topSymbols).contains(buttonValue)) {
+                if (buttonValue == "AC") {
+                    clearAll();
+                    displayLabel.setText("0");
+                }
+                else if (buttonValue == "+/-") {
+                    double numDisplay = Doucble.parseDouble(displayLabel.getText());
+                    numDisplay *= -1;
+                    displayLabel.setText(removeZeroDecimal(numDisplay));
+                }
+                else if (buttonValue == "%") {
+                    double numDisplay = Doucble.parseDouble(displayLabel.getText());
+                    numDisplay /= 100;
+                    displayLabel.setText(removeZeroDecimal(numDisplay));
+
+        } else { //digits or .
+            if (buttonValue == ".") {
+                if (!displayLabel.getText().contains(buttonValue)) {
+                    displayLabel.setText(displayLabel.getText() + buttonValue);
+
+            } else if ("0123456789".contains(buttonValue)) {
+                if (displayLabel.getText() == "0") {
+                    displayLabel.setText(buttonValue);
+                } else {
+                    displayLabel.setText(displayLabel.getText() + buttonValue);
+                }
+            }
+        }
+    }
+});
+
 }
+
+    void clearAll() {
+        A = "0";
+        operator = null;
+        B = null;
+    }
+    String removeZeroDecimal(double numDisplay) {
+        if (numDisplay % 1 == 0) {
+            return Integer.toString((int) numDisplay);
+        }
+        return Double.toString(numDisplay);
+    }
+        }
+
 
